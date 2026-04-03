300023ce914538279f8e6fc482bc89efef877d47
MuratYucedag <yucedagmurat23@gmail.com>
2026-04-03 14:57:05 +0300

    Ders 34

--- FlightBooking/Areas/Admin/Controllers/FlightsController.cs
+++ FlightBooking/Areas/Admin/Controllers/FlightsController.cs
@@ -30,5 +30,10 @@ namespace FlightBooking.Areas.Admin.Controllers
             await _flightService.CreateFlightAsync(createFlightDto);
             return RedirectToAction("FlightList");
         }
+
+        public async Task<IActionResult> FlightDetail(string id)
+        {
+            return View();
+        }
     }
 }
